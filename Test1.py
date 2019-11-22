bacbf994ce6d6fd6f616f16bd771a68d68168311
Kuroashi101 <shadowpaw101@gmail.com>
2019-11-21 22:02:21 -0500

    Update Test1.py

--- Test1.py
+++ Test1.py
@@ -3,7 +3,7 @@ print ('Hello World')
 print ('What is your name?') #Ask for name first
 myName = input()
 print ('It is good to meet you, ' + myName)
-print ('The length of your name is:' + (len(myName)) + ('letters.'))
+print ('The length of your name is:')
 print (len(myName))
 print ('What is your age?') #ask for their age
 myAge = input()
